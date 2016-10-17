020b727a5b0be215af6de9776229cb2d1ec9dbd7
Ferch2109 <marifer.gusanito@gmail.com>
2016-10-16 22:43:57 -0500

    Ya se actualizan las filas y columnas.

--- servidor.py
+++ servidor.py
@@ -18,33 +18,49 @@ class Estado(Enum):
 	NINGUNO = 7
 
 class Servidor(QtGui.QMainWindow):
-   
-    def __init__(self):
-    	super( Servidor, self ).__init__()
-    	uic.loadUi( 'servidor.ui', self )
-    	self.redimensionar()
-    	self.poner_lienzo_celdas()
-    	self.estado = Estado.NINGUNO
+
+	def __init__(self):
+		super( Servidor, self ).__init__()
+		uic.loadUi( 'servidor.ui', self )
+		self.redimensionar()
+		self.poner_lienzo_celdas()
+		self.clickers()
+		self.estado = Estado.NINGUNO
+    	#self.principal.setWidgetResizable(True)
 
 
-    def redimensionar(self):
-    	self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
-    	self.table.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+	def redimensionar( self ):
+		self.tabla.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+		self.tabla.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
 
 
-    def poner_lienzo_celdas(self):
-    	self.table.setSelectionMode(QtGui.QTableWidget.NoSelection) 
-    	for i in range( self.table.rowCount() ) :
-    		for j in range( self.table.columnCount() ) :
-    			self.table.setItem( i, j, QtGui.QTableWidgetItem() )
-    			self.table.item( i, j ).setBackground( QtGui.QColor( 0, 0, 0 ) )
+	def poner_lienzo_celdas( self ):
+		self.tabla.setSelectionMode(QtGui.QTableWidget.NoSelection) 
+		for i in range( self.tabla.rowCount() ) :
+			for j in range( self.tabla.columnCount() ) :
+				self.tabla.setItem( i, j, QtGui.QTableWidgetItem() )
+				self.tabla.item( i, j ).setBackground( QtGui.QColor( 0, 0, 0 ) )
 
+
+	def clickers( self ):
+		self.columnas.valueChanged.connect( self.cambio_numero_celdas )
+		self.filas.valueChanged.connect( self.cambio_numero_celdas )
+		#self.espera.valueChanged.connect(self.actualizar_espera)
+
+
+	def cambio_numero_celdas( self ):
+		filas = self.filas.value()
+		columnas = self.columnas.value()
+		self.tabla.setRowCount(filas)
+		self.tabla.setColumnCount(columnas)
+		self.poner_lienzo_celdas()
+		self.redimensionar()
 
 
 class Snake():
 	def __init__( self, r, g, b ):
 		self.lista = [[0,0],[1,0],[2,0],[3,0],[4,0]]
-		self.tamanio = len(self.lista)
+		self.tamanio = len( self.lista )
 		self.direccion = Dir.ABAJO
 		self.color = (r,g,b)
 
